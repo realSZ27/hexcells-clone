7207416b327174d2d92b2c34fb6162c86e100ce3
Karsten Fischer <fiskar02@ISD719.lan>
2026-04-20 11:11:58 -0500

    eee

--- scripts/level.gd
+++ scripts/level.gd
@@ -7,14 +7,24 @@ var cell_scene := preload("res://cell.tscn")
 var map: Map
 
 var mistakes_count := 0
+var remaining := 0
 
 # visual config
 const HEX_SIZE := 20.0
 
 func _ready() -> void:
 	map = Map.from_file("res://levels/thetrial.hexcells")
+	
 	Autoload.increment_mistakes.connect(_increment_mistakes)
 	queue_redraw()
+	
+func _process(_delta: float) -> void:
+	map.for_each_cell(count_blue_cells)
+	
+func count_blue_cells(cell: CellData, _col: int, _row: int) -> void: 
+	if cell.is_blue():
+		remaining += 1
+	
 
 func _draw() -> void:
 	map.for_each_cell(draw_cell)
@@ -31,4 +41,4 @@ func draw_cell(cell: CellData, col: int, row: int) -> void:
 
 func _increment_mistakes() -> void:
 	mistakes_count += 1
-	$Label.text = str(mistakes_count)
+	$UI/VBoxContainer/Mistakes.text = str(mistakes_count)
